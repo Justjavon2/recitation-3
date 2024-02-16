0a110b2452d210fe6641e068c57fb296dfde2eeb
Jmedmunds <Jmedmunds23@students.desu.edu>
2024-02-16 15:41:22 -0500

    ah dreads done

--- src/main/java/Recitation3.java
+++ src/main/java/Recitation3.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 
 public class Recitation3 {
-    public static void main(String[] argv)
-    {
-        Scanner scan = new Scanner(System.in);
+    public static void main(String[] argv) {
+        Scanner vergil = new Scanner(System.in);
 
-        // add your code here
+        int x = vergil.nextInt();
+        int y = vergil.nextInt();
+
+        Circle circle16 = new Circle(new Point(x, y), 16);
+
+        Circle circle40 = new Circle(new Point(14, 12), 40);
+
+        if (circle16.intersects(circle40)) {
+            System.out.println("Circles intersect!");
+
+        } else {
+            System.out.println("Circles do not intersect!");
+        }
+
+
     }
 }
